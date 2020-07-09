fbcda3684a9a0d55de6d698b150ba90ec96c7e49
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2020-07-09 15:31:31 +0800

    initial commit

---
+++ wenprise-assets.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:        Wenprise Assets
+Plugin URI:         https://www.wpzhiku.com/
+Description:        Most used assets in WordPress theme development.
+Version:            1.0.0
+Author:             WordPress 智库
+Author URI:         https://www.wpzhiku.com/
+License:            MIT License
+License URI:        http://opensource.org/licenses/MIT
+*/
+
+require_once(plugin_dir_path(__FILE__) . 'vendor/autoload.php');
